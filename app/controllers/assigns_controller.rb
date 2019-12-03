dbda08839207c6f3bbb280ebf89f768e0de43c6f
shuhei akamine <aeiueoaoaiueo@gmail.com>
2019-12-04 08:33:54 +0900

    team userの削除権限の修正

--- app/controllers/assigns_controller.rb
+++ app/controllers/assigns_controller.rb
@@ -30,6 +30,8 @@ class AssignsController < ApplicationController
       I18n.t('views.messages.cannot_delete_the_leader')
     elsif Assign.where(user_id: assigned_user.id).count == 1
       I18n.t('views.messages.cannot_delete_only_a_member')
+    elsif current_user != assign.team.owner && current_user != assign.user
+      redirect_to team_url(params[:team_id]), I18n.t('views.messages.cannot_delete_but_owner_or_the_user')
     elsif assign.destroy
       set_next_team(assign, assigned_user)
       I18n.t('views.messages.delete_member')
